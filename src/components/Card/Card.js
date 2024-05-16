d677333c3f1cb0c9995a8c75f540ff177a722f62
Ebrahim mamdouh <138860098+ebrahim-mamdoh@users.noreply.github.com>
2024-05-16 21:49:52 +0300

    finish moast POP

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,21 +1,22 @@
 import './Card.css'
 import popular_1 from '../../assets/images/popular-01.jpg'
 
+import {FaStar ,FaDownload} from 'react-icons/fa'
 
 
 function Card(props) {
   return (
 <div className='most-popular-item'>
               <div className='card-wrapper'>
-                  <img className='most-popular-img' src={popular_1}/>
+                  <img className='most-popular-img' src={props.img}/>
                   <div className='most-popualr-content'>
                     <h4 className='most-popular-title'>
                       {props.title} <br/>
                       <span>Sandbox</span>
                     </h4>
                     <ul>
-                        <li><span>4.8</span></li>
-                        <li><span>2.3M</span></li>
+                        <li> <span style={{"color":'yellow'}}><FaStar/></span> <span>4.8</span></li>
+                        <li> <span  style={{"color":'var(--color-primary)'}}><FaDownload/></span> <span>2.3M</span></li>
                       </ul>
                   </div>
               </div>
--- src/components/sections/MostPop/MostPop.js
+++ src/components/sections/MostPop/MostPop.js
@@ -1,5 +1,9 @@
 import './MostPop.css'
 import Card from '../../Card/Card'
+import popular_1 from '../../../assets/images/popular-01.jpg'
+import popular_2 from '../../../assets/images/popular-02.jpg'
+import popular_3 from '../../../assets/images/popular-03.jpg'
+import popular_4 from '../../../assets/images/popular-04.jpg'
 
 
 
@@ -12,11 +16,14 @@ function MostPop() {
             <h4>Most popular</h4>
         </div>
         <div className='most-popular-items'>
-            <Card title='Fortnite'  category='sandbox'     rate='3.8'  downloas='4.5M'/>
-            <Card title='Pubg'      category='stream-x'    rate='4.8'  downloas='1.1M'/>
-            <Card title='Dota2'     category='legendary'   rate='3.5'  downloas='5.3M'/>
-            <Card title='Cs-Go'     category='battle 5'    rate='4.6'  downloas='6.1M'/>
-        
+            <Card img={popular_1} title='Fortnite'  category='sandbox'     rate='3.8'  downloas='4.5M'/>
+            <Card img={popular_2} title='Pubg'      category='stream-x'    rate='4.8'  downloas='1.1M'/>
+            <Card img={popular_3} title='Dota2'     category='legendary'   rate='3.5'  downloas='5.3M'/>
+            <Card img={popular_4} title='Cs-Go'     category='battle 5'    rate='4.6'  downloas='6.1M'/>
+            <Card img={popular_1} title='Fortnite'  category='sandbox'     rate='3.8'  downloas='4.5M'/>
+            <Card img={popular_2} title='Pubg'      category='stream-x'    rate='4.8'  downloas='1.1M'/>
+            <Card img={popular_3} title='Dota2'     category='legendary'   rate='3.5'  downloas='5.3M'/>
+            <Card img={popular_4} title='Cs-Go'     category='battle 5'    rate='4.6'  downloas='6.1M'/>
          
         </div>
       </div>
